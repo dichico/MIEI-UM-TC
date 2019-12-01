7066b772fae71414fc787d77463d197feb99adac
dichico <36507885+dichico@users.noreply.github.com>
2019-12-01 18:53:58 +0000

    mais trocas

--- Guiões/G9/OpenSSLWorker.py
+++ Guiões/G9/OpenSSLWorker.py
@@ -1,6 +1,6 @@
 from OpenSSL import crypto
 
-with open('CAPEM.cer', 'r') as ca_file:
+with open('Certificados/CAPEM.cer', 'r') as ca_file:
     ca_pem = ca_file.read()
 
 
@@ -12,7 +12,7 @@ store.add_cert(ca)
 def clientCertVerify():
     
     # Abertura do PKCS12 com o certificado do cliente lá dentro.
-    with open('Cliente1.p12', 'rb') as client_file:
+    with open('Certificados/Cliente1.p12', 'rb') as client_file:
         clientPKCS12 = client_file.read()
     
     p12Client = crypto.load_pkcs12(clientPKCS12, bytes('1234', 'utf-8'))
@@ -31,7 +31,7 @@ def clientCertVerify():
 def serverCertVerify():
     
     # Abertura do PKCS12 com o certificado do cliente lá dentro.
-    with open('Servidor.p12', 'rb') as servidor_file:
+    with open('Certificados/Servidor.p12', 'rb') as servidor_file:
         servidorPKCS12 = servidor_file.read()
     
     p12Servidor = crypto.load_pkcs12(servidorPKCS12, bytes('1234', 'utf-8'))
